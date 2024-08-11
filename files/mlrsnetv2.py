eb7aae6cff56941a21b6e1807755f04200e2976e
Mainak Singha <117339425+mainaksingha01@users.noreply.github.com>
2024-08-12 00:34:22 +0900

    Update mlrsnetv2.py

--- files/mlrsnetv2.py
+++ files/mlrsnetv2.py
@@ -123,7 +123,7 @@ class MLRSNetv2(DatasetBase):
             self.dataset_dir, "mlrsnetv2.json")
         self.shots_dir = os.path.join(
             self.dataset_dir, "shots") 
-        # mkdir_if_missing(self.shots_dir)
+        mkdir_if_missing(self.shots_dir)
 
         if os.path.exists(self.split_path):
             train, val, test = read_split(
