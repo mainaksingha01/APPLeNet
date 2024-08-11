e127e3b148c88ddec59b8a6bdf015c604fab3a79
Mainak Singha <117339425+mainaksingha01@users.noreply.github.com>
2024-08-12 00:41:05 +0900

    Update applenet.py

--- models/applenet.py
+++ models/applenet.py
@@ -47,21 +47,12 @@ class AdaIN_trans(nn.Module):
 				super().__init__()
 
 		def mu(self, x):
-				""" Takes a (n,c,h,w) tensor as input and returns the average across
-				it's spatial dimensions as (h,w) tensor [See eq. 5 of paper]"""
 				return torch.sum(x,(1))/(x.shape[1])
 
 		def sigma(self, x):
-				""" Takes a (n,c,h,w) tensor as input and returns the standard deviation
-				across it's spatial dimensions as (h,w) tensor [See eq. 6 of paper] Note
-				the permutations are required for broadcasting"""
 				return torch.sqrt((torch.sum((x.permute([1,0,2])-self.mu(x)).permute([1,0,2])**2,(1))+0.000000023)/(x.shape[1]))
 
 		def forward(self, x, y):
-				""" Takes a content embeding x and a style embeding y and changes
-				transforms the mean and standard deviation of the content embedding to
-				that of the style. [See eq. 8 of paper] Note the permutations are
-				required for broadcasting"""
 				return (self.sigma(y)*((x.permute([1,0,2])-self.mu(x))/self.sigma(x)) + self.mu(y)).permute([1,0,2])
 
 # for ViT :
