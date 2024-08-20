6819a9b844f5e7e2bc98071ac9538c4a02d7c837
Mainak Singha <117339425+mainaksingha01@users.noreply.github.com>
2024-08-20 15:37:37 +0900

    Update clip.py

--- clip/clip.py
+++ clip/clip.py
@@ -9,7 +9,7 @@ from PIL import Image
 from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
 from tqdm import tqdm
 
-from .model_modified import build_model
+from .model import build_model
 from .simple_tokenizer import SimpleTokenizer as _Tokenizer
 
 try:
